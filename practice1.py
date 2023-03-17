90b37ba64dd2e3619247c1e3167aaba6a2661993
Emrah <emrhtrk64@gmail.com>
2023-03-17 17:04:32 +0300

    v1

---
+++ practice1.py
@@ -0,0 +1,10 @@
+a=input("Lütfen bir sayı giriniz?")
+if a.isdigit():
+    if int(a) in [0,1,2,3,4,5,6,7,8,9]:
+        sonuc=int(a) + int(a*2) + int(a*3)
+    else:
+        sonuc="Lütfen geçerli bir sayı giriniz"
+else:
+    sonuc="Lütfen Geçerli bir sayı giriniz"
+
+print(sonuc)
